afafb815ba24c17a2a44f139deefcdabe23f1030
Robert Musial <rmusial@users.noreply.github.com>
2016-08-22 16:45:08 -0400

    Update mbpfan.c
    Added my contact info

--- src/mbpfan.c
+++ src/mbpfan.c
@@ -4,6 +4,7 @@
  *  Modifications by Rafael Vega <rvega@elsoftwarehamuerto.org>
  *  Modifications (2012) by Daniel Graziotin <daniel@ineed.coffee>
  *  Modifications (2012) by Ismail Khatib <ikhatib@gmail.com>
+ *  Modifications (2016) by Robert Musial <rmusial@fastmail.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
